cd8a23f3b7e2274d829092c7f6c2e5a0a4100c3c
nirr <nrr235@163.com>
2018-11-26 13:37:00 +0800

    url token

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,29 @@
   import 'babel-polyfill';
   import FastClick from 'fastclick';
 
+  function getArgs (url) {
+    let args = {}
+    let query = url.substr(url.indexOf('?') + 1)
+    let pairs = query.split('&')
+    for (let i = 0; i < pairs.length; i++) {
+      let pos = pairs[i].indexOf('=')
+      if (pos === -1) continue
+      let argname = pairs[i].substring(0, pos)
+      let value = pairs[i].substring(pos + 1)
+      value = decodeURIComponent(value)
+      args[argname] = value
+    }
+    return args
+  }
+
   FastClick.attach(document.body);
   export default {
     name: 'app',
     created () {
-      if (this.$route.query.token) {
-        this.$store.commit('BASE_LOGIN', {user_token: this.$route.query.token})
+      const args = getArgs(window.location.href)
+      if (args.token) {
+        if (args.token.lastIndexOf('#/') !== -1) args.token = args.token.substring(0, args.token.lastIndexOf('#/'))
+        this.$store.commit('BASE_LOGIN', {user_token: args.token})
         this.$store.dispatch('MINE_INFO')
       }
     }
